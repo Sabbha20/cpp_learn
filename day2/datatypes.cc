d7c10e228577093b88d44406963332e118e8cb17
Sabbha20 <sabbha.power1@gmail.com>
2026-07-13 19:47:05 +0530

    day2:datatypes

---
+++ day2/datatypes.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstdint>
+
+int main(){
+    std::int8_t a = 0;
+    std::uint8_t b = 0U;
+
+    std::int16_t i1 = 0;
+    std::uint16_t i1 = 0U;
+
+    std::int32_t i1 = 0;
+    std::uint32_t i1 = 0U;
+
+    std::int64_t i1 = 0;
+    std::uint64_t i1 = 0U;
+
+    float f1 = 12.0f;
+    double d1 = 12.0;
+
+    char c1 = 'A';
+    char c2 = 'z';
+
+    bool b1 = true;
+    bool b2 = false;
+}
